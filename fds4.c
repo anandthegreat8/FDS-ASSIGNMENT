676f0edcbabc638ac91341f95c256fbb64983f39
anandthegreat8 <43315386+anandthegreat8@users.noreply.github.com>
2019-07-22 11:56:07 +0530

    Create fds4.c

---
+++ fds4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct student
+{
+  int roll_no;
+  char name[100];
+  float fee;
+  long int mobile_no;
+}
+
+int main()
+{
+  printf("ENTER YOUR CHOICE:\n1.CREATE DATABASE\n2.DISPLAY DATABASE\n3.", );
+}
